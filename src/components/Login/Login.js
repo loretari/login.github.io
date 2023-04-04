701f0cba2f03f8773a757632199d6cf9cf2dc10c
loretari <loretari@gmail.com>
2023-04-04 14:47:25 +0300

    init: add useReducer & useEffect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -44,6 +44,10 @@ const Login = (props) => {
         isValid: null,
     })
 
+     const { isValid: emailIsValid} = emailState;
+    const { isValid: passwordIsValid} = passwordState;
+
+
     useEffect(() => {
         console.log('EFFECT RUNNING');
 
@@ -52,40 +56,40 @@ const Login = (props) => {
         }
     }, [])
 
-    // useEffect(() => {
-    //     const identifier = setTimeout(() => {
-    //         console.log('Checking form validity!')
-    //         setFormIsValid (
-    //             enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    //         )
-    //     }, 500);
-    //
-    //     return () => {
-    //         console.log('CLEANUP');
-    //         clearTimeout(identifier);
-    //     };
-    //
-    // }, [enteredEmail, enteredPassword])
+    useEffect(() => {
+        const identifier = setTimeout(() => {
+            console.log('Checking form validity!')
+            setFormIsValid (
+                emailIsValid && passwordIsValid
+            )
+        }, 500);
+
+        return () => {
+            console.log('CLEANUP');
+            clearTimeout(identifier);
+        };
+
+    }, [emailIsValid, passwordIsValid])
 
     const emailChangeHandler = (event) => {
         dispatchEmail({type: 'USER_INPUT', val: event.target.value})
         // setEnteredEmail(event.target.value)
 
-        setFormIsValid (
-            event.target.value.includes('@') && passwordState.isValid
-
-
-        )
+        // setFormIsValid (
+        //     event.target.value.includes('@') && passwordState.isValid
+        //
+        //
+        // )
     }
 
     const passwordChangeHandler = (event) => {
         dispatchPassword({type: 'USER_INPUT', val: event.target.value})
 
-        setFormIsValid (
-            // event.target.value.trim().length > 6 && enteredEmail.includes('@')
-           emailState.isValid && event.target.value.trim().length > 6
-
-        )
+        // setFormIsValid (
+        //     // event.target.value.trim().length > 6 && enteredEmail.includes('@')
+        //    emailState.isValid && event.target.value.trim().length > 6
+        //
+        // )
     }
 
 
